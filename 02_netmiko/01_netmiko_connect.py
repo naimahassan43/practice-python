ba0eecb2484fc19599f102c7bea250c729bb7215
Naima Hassan <naima.hassan434@gmail.com>
2025-12-08 12:08:02 +0000

    Netmiko-enable and global config mode

--- 02_netmiko/01_netmiko_connect.py
+++ 02_netmiko/01_netmiko_connect.py
@@ -15,9 +15,26 @@ cisco_device = {
     }
 
 connection = ConnectHandler(**cisco_device)
-output = connection.send_command('sh ip int brief')
+
+prompt = connection.find_prompt()
+
+if '>' in prompt:
+    connection.enable()
+    
+output = connection.send_command('sh ip int br')
 print(output)
 
+if not connection. check_config_mode():
+    connection.config_mode()
+    
+######### Enter global config mode####### 
+#print(connection. check_config_mode())
+connection.send_command('username user1 secret cisco')
+
+######### Exit global config mode#######
+connection.exit_config_mode()
+print(connection. check_config_mode())
+print(connection.send_command('sh run'))
 ####### Disconnecting Device ##########
 print('Closing Connection')
 connection.disconnect()
